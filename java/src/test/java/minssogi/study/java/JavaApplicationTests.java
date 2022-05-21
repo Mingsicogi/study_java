ca8d4d03bad64467bc1c5f308d84ddd2fa9f1e20
Mingsicogi <codedoctor119@gmail.com>
2022-05-21 19:57:11 +0900

    Modify basic structure of java study package

--- java/src/test/java/minssogi/study/java/JavaApplicationTests.java
+++ java/src/test/java/minssogi/study/java/JavaApplicationTests.java
@@ -3,7 +3,6 @@ package minssogi.study.java;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JavaApplicationTests {
 
     @Test
