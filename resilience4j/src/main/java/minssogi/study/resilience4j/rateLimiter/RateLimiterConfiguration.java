77b649b306f78a76351e42ba9bca5e0358c84b50
Mingsicogi <codedoctor119@gmail.com>
2022-05-29 16:10:50 +0900

    Add RateLimiter of Resilience4j study

--- resilience4j/src/main/java/minssogi/study/resilience4j/rateLimiter/RateLimiterConfiguration.java
+++ resilience4j/src/main/java/minssogi/study/resilience4j/rateLimiter/RateLimiterConfiguration.java
@@ -17,9 +17,9 @@ public class RateLimiterConfiguration {
 
     private RateLimiterConfig rateLimiterConfig() {
         return RateLimiterConfig.custom()
-                .timeoutDuration(Duration.ofMillis(10))
-                .limitRefreshPeriod(Duration.ofSeconds(3))
-                .limitForPeriod(10)
+                .timeoutDuration(Duration.ofMillis(10)) // 초과된 요청을 얼만큼 기다릴지 시간 설정.
+                .limitRefreshPeriod(Duration.ofSeconds(3)) // limit for period 설정이 초기화되는 주기.
+                .limitForPeriod(10) // limit refresh period 로 refresh 되기전에 허용되는 request 수
                 .writableStackTraceEnabled(true)
                 .build();
     }
