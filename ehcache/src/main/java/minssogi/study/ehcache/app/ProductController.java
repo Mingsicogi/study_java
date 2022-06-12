22b2a925a382e853058c2eaaa03b7cbbd942f70e
Mingsicogi <codedoctor119@gmail.com>
2022-06-13 01:51:09 +0900

    Add web dependency on ehcache sample project

--- ehcache/src/main/java/minssogi/study/ehcache/app/ProductController.java
+++ ehcache/src/main/java/minssogi/study/ehcache/app/ProductController.java
@@ -1,5 +1,12 @@
 package minssogi.study.ehcache.app;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
 }
