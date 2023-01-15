53190561aba29ff3e0a9a3d991fcb0c6d537a3fb
Mingsicogi <codedoctor119@gmail.com>
2023-01-15 20:09:27 +0900

    http2 server push function test

--- server_push/src/main/java/minssogi/study/server_push/app/fcm/WebPush.java
+++ server_push/src/main/java/minssogi/study/server_push/app/fcm/WebPush.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Slf4j
