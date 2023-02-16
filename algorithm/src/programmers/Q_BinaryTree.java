4b60f980b42f1f5b78596eb30256c49b8a22ea8d
Mingsicogi <codedoctor119@gmail.com>
2023-02-17 00:54:33 +0900

    add algorithm study

---
+++ algorithm/src/programmers/Q_BinaryTree.java
@@ -0,0 +1,19 @@
+package programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/150367
+ *
+ * @author minssogi
+ */
+public class Q_BinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(long[] numbers) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
