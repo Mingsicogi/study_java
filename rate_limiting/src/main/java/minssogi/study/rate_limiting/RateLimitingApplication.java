b461f197f2af6a30b73a5dcc537234ade3842636
전민석 <minssogi@line.games>
2023-03-12 23:39:08 +0900

    add rate limiting example

---
+++ rate_limiting/src/main/java/minssogi/study/rate_limiting/RateLimitingApplication.java
@@ -0,0 +1,16 @@
+package minssogi.study.rate_limiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class RateLimitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitingApplication.class, args);
+    }
+
+}
