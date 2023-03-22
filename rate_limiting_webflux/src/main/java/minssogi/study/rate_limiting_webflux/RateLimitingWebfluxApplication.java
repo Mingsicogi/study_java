93db1227643841306e98e9a59a1778ae57b9da62
전민석 <codedoctor119@gmail.com>
2023-03-22 11:45:09 +0900

    add rate limiting example  + append using webflux example

---
+++ rate_limiting_webflux/src/main/java/minssogi/study/rate_limiting_webflux/RateLimitingWebfluxApplication.java
@@ -0,0 +1,13 @@
+package minssogi.study.rate_limiting_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitingWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitingWebfluxApplication.class, args);
+    }
+
+}
---
+++ rate_limiting_webflux/src/test/java/minssogi/study/rate_limiting_webflux/RateLimitingWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package minssogi.study.rate_limiting_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
