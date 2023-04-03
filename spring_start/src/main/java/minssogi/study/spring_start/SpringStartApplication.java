c9862f09fb1d2741c63c17f6993a04bb5651d058
전민석 <codedoctor119@gmail.com>
2023-04-03 11:51:54 +0900

    springboot core study

--- spring_start/src/main/java/minssogi/study/spring_start/SpringStartApplication.java
+++ spring_start/src/main/java/minssogi/study/spring_start/SpringStartApplication.java
@@ -1,15 +1,12 @@
 package minssogi.study.spring_start;
 
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringStartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStartApplication.class, args);
+
     }
 
 }
