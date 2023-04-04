e6dbcc341e11aeb56beeb92ab5a92be9863fc3c2
전민석 <codedoctor119@gmail.com>
2023-04-04 13:34:25 +0900

    servlet container 를 직접 사용해 서버 start

--- spring_start/src/main/java/minssogi/study/spring_start/SpringStartApplication.java
+++ spring_start/src/main/java/minssogi/study/spring_start/SpringStartApplication.java
@@ -1,12 +1,37 @@
 package minssogi.study.spring_start;
 
-import org.springframework.boot.SpringApplication;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import java.io.IOException;
 
 //@SpringBootApplication
 public class SpringStartApplication {
 
     public static void main(String[] args) {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
 
+
+        WebServer tomcatWebServer = tomcatServletWebServerFactory.getWebServer(new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.addServlet("HelloController", new HttpServlet() {
+                    @Override
+                    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                        resp.getWriter().write("Hello World");
+                    }
+
+                }).addMapping("/hello");
+            }
+        });
+
+        tomcatWebServer.start();
     }
 
 }
